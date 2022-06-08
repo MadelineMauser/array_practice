a7ea3b1ca596fd38c2aa8bc0881d85b372c51f39
Madeline Mauser <madeline.mauser@gmail.com>
2022-06-08 10:03:41 -0600

    Add array methods and comments

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,20 @@ dog_ages = [7, 3, 2, 6]
 food_prices = [10.75, 6.30, 5.25, 8.00]
 
 had_walks = [true, true, false, false]
+
+
+# This array method will remove the element "Cinder"
+# from the array and return its value.
+dogs.pop
+
+# This array method will remove the element 7
+# from the array and return its value.
+dog_ages.shift
+
+# This array method will add 3.40 to the beginning of the array
+# and return the whole array.
+food_prices.unshift(3.40)
+
+# This array method will add true to the end of the array 
+# and return the whole array.
+had_walks.push(true)
